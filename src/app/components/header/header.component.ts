be27bea0756cdf8375e976b97d68383019f8f6be
AugustoCVS <guty.vertolis@gmail.com>
2024-04-15 20:20:16 -0300

    feat: add EventEmitter screen change

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Output } from '@angular/core'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart, faHouse } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,12 +11,14 @@ import { faHeart, faHouse } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @Output() screenChange = new EventEmitter<string>();
+
   faHeart = faHeart;
   faHouse = faHouse;
-
   currentScreen: string = 'inicio';
 
   handleScreenChange(screen: string): void {
     this.currentScreen = screen;
+    this.screenChange.emit(screen);
   }
 }
