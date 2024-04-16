b6e3f512502105395537a6738af979995b67ebbc
AugustoCVS <guty.vertolis@gmail.com>
2024-04-16 14:32:49 -0300

    feat: create getCharacterList and handleSearch features

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CharacterService } from '../../services/character.service';
+import { ICharacter } from '../../@types/character.interface';
 import { TitleComponent } from '../common/title/title.component';
-import { InputComponent } from '../common/input/input.component';
 import { NotFoundComponent } from '../common/not-found/not-found.component';
+import { InputComponent } from '../common/input/input.component';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs/operators';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TitleComponent, InputComponent, NotFoundComponent],
+  imports: [TitleComponent, InputComponent, NotFoundComponent, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  characterList = [{}];
+export class HomeComponent implements OnInit {
+  characterList: ICharacter[] = [];
+  search = new FormControl('');
+  loading = signal(false);
 
+  constructor(private characterService: CharacterService) {
+    this.handleSearch();
+  }
 
-  handleSearch({ value }: { value: string }): void {
-    console.log('TESTE', value);
+  getCharacterList(): void {
+    this.loading.set(true);
+    this.characterService.getCharacters().subscribe({
+      error: (err) => {
+        this.characterList = [];
+        console.error(err);
+      },
+      next: (res) => {
+        this.characterList = res.results;
+      },
+      complete: () => this.loading.set(false),
+    });
+  }
+
+  handleSearch(): void {
+    this.search.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        switchMap((value) =>
+          this.characterService.getCharacterByName({ name: value }).pipe(
+            catchError(() => {
+              return (this.characterList = []);
+            })
+          )
+        )
+      )
+      .subscribe({
+        next: (res) => {
+          this.characterList = res.results;
+        },
+      });
+  }
+
+  ngOnInit(): void {
+    this.getCharacterList();
   }
 }
