e773aff6cc59d514dac46b2845d9c7f52bd7e069
AugustoCVS <guty.vertolis@gmail.com>
2024-04-16 17:26:13 -0300

    feat: create route for favorite screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'favoritos',
+    component: FavoriteComponent
   }
 ];
