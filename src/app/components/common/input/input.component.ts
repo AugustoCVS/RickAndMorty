ef5669fc2e8571e0a7a1627f5e4c4ce1ff79d48d
AugustoCVS <guty.vertolis@gmail.com>
2024-04-16 14:32:06 -0300

    reactor: change input logic and props

--- src/app/components/common/input/input.component.ts
+++ src/app/components/common/input/input.component.ts
@@ -1,19 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss'
+  styleUrl: './input.component.scss',
 })
 export class InputComponent {
-  @Input() placeholder: string = "";
-  @Input() value: string = "";
-  @Output() searchValue = new EventEmitter<string>();
-
-  onInput({event}: {event: Event}): void {
-    const value = (event.target as HTMLInputElement).value;
-    this.searchValue.emit(value);
-  }  
+  @Input() placeholder: string = '';
+  @Input() searchControl: FormControl = new FormControl();
 }
