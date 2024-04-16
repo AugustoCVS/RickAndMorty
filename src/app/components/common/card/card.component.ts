ddf76a5c52853d3a72d2850692dbc97e3e07eb9d
AugustoCVS <guty.vertolis@gmail.com>
2024-04-16 15:07:26 -0300

    refactor: ajuste temporário no icone

--- src/app/components/common/card/card.component.ts
+++ src/app/components/common/card/card.component.ts
@@ -2,7 +2,7 @@ import { NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
-import { IconDefinition, faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-card',
@@ -12,7 +12,6 @@ import { IconDefinition, faHeart as solidHeart } from '@fortawesome/free-solid-s
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() isFavorite: boolean = false
   @Input() imgSrc: string = '';
   @Input() name: string = '';
   @Input() species: string = '';
@@ -20,12 +19,4 @@ export class CardComponent {
 
   solidHeart = solidHeart;
   emptyHeart = regularHeart;
-
-  toggleFavorite() {
-    this.isFavorite = !this.isFavorite;
-  }
-
-  handleRenderIcon(): IconDefinition {
-    return this.isFavorite ? this.solidHeart : this.emptyHeart;
-  }
 }
