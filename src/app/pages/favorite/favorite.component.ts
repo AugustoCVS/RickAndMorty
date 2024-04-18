4ff13c7ffbdb4326c5d3b8063bfe286f1569708a
AugustoCVS <guty.vertolis@gmail.com>
2024-04-18 14:38:37 -0300

    feat: turning the favoritesCharacters$ into a observable

--- src/app/pages/favorite/favorite.component.ts
+++ src/app/pages/favorite/favorite.component.ts
@@ -3,25 +3,24 @@ import { ICharacter } from '../../@types/character.interface';
 import { CardComponent } from '../../components/card/card.component';
 import { NotFoundComponent } from '../../components/not-found/not-found.component';
 import { TitleComponent } from '../../components/title/title.component';
-import { NgForOf, NgIf } from '@angular/common';
+import { CommonModule, NgForOf, NgIf } from '@angular/common';
 import { FavoriteService } from '../../core/services/Favorite/favorite.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-favorite',
   standalone: true,
-  imports: [TitleComponent, NotFoundComponent, CardComponent, NgIf, NgForOf],
+  imports: [TitleComponent, NotFoundComponent, CardComponent, NgIf, NgForOf, CommonModule],
   templateUrl: './favorite.component.html',
   styleUrl: './favorite.component.scss',
 })
 export class FavoriteComponent {
-  favoritesCharacters: ICharacter[] = [];
+  favoritesCharacters$: Observable<ICharacter[]> = new Observable<ICharacter[]>();
 
   constructor(private favoriteService: FavoriteService) {}
 
   handleGetFavoriteList(): void {
-    this.favoriteService.getFavoriteList().subscribe((characters) => {
-      this.favoritesCharacters = characters;
-    });
+   this.favoritesCharacters$ = this.favoriteService.getFavoriteList();
   }
 
   ngOnInit(): void {
