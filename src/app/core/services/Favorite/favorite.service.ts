5092f442223a15baf6d97324d70f91958085459f
AugustoCVS <guty.vertolis@gmail.com>
2024-04-18 00:16:51 -0300

    feat: create favoriteService service to control the favorite list

--- src/app/core/services/Favorite/favorite.service.ts
+++ src/app/core/services/Favorite/favorite.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
+import { ICharacter } from "../../../@types/character.interface";
 
 @Injectable({
   providedIn: 'root',
 })
 export class FavoriteService{
-  private favoriteCharacters = new BehaviorSubject<string[]>([]);
-  favoriteCharacters$ = this.favoriteCharacters.asObservable();
+  private favoriteListSubject: BehaviorSubject<ICharacter[]> = new BehaviorSubject<ICharacter[]>([]);
+  favoriteList$: Observable<ICharacter[]> = this.favoriteListSubject.asObservable();
 
-  addCharacter(character: string): void {
-    const currentValue = this.favoriteCharacters.value;
-    if (!currentValue.includes(character)) {
-      this.favoriteCharacters.next([...currentValue, character]);
-    }
+  getFavoriteList(): Observable<ICharacter[]> {
+    return this.favoriteList$;
   }
 
-  removeCharacter(character: string): void {
-    const currentValue = this.favoriteCharacters.value;
-    const updatedValue = currentValue.filter((c) => c !== character);
-    this.favoriteCharacters.next(updatedValue);
+  addFavorite(character: ICharacter): void {
+    const currentList = this.favoriteListSubject.getValue();
+    this.favoriteListSubject.next([...currentList, character]);
   }
 
+  removeFavorite(character: ICharacter): void {
+    const currentList = this.favoriteListSubject.getValue();
+    const newList = currentList.filter((item) => item.id !== character.id);
+    this.favoriteListSubject.next(newList);
+  }
+
+  isFavorite(character: ICharacter): boolean {
+    const currentList = this.favoriteListSubject.getValue();
+    return currentList.some((item) => item.id === character.id);
+  }
 }
